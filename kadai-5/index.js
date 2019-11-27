60c66070154e7224e836a79fc9bcbb223e978ee6
KitamuraNana <ktamuran@gmail.com>
2019-11-27 05:13:03 +0000

    191127

--- kadai-5/index.js
+++ kadai-5/index.js
@@ -2,7 +2,18 @@ const profile = {
     name: 'HACHI',
     age: 8,
     hobby: 'books',
-    major:'asia'
-}
+    major: 'asia'
+};
 
-for
+    
+
+console.log('わたしの名前は' + profile.name + 'です。')
+
+const keys = Object.keys(profile);
+
+for (let i = 0; i < keys.length; i = i + 1) {
+    const key = keys[i];
+    const value = profile[key];
+    const text = 'watashsino' + key + 'ha' + value + 'des'
+    console.log(text);
+}
---
+++ kadai-6/index-2.js
@@ -0,0 +1,53 @@
+const str1 = "abc"
+console.log(JSON.parse(str1)) 
+//str1のパース結果
+    try {
+        console.log(JSON.parse(str1))
+    } catch (error) {
+         console.log(error)
+    //kokoniriyu wo kakude
+
+    }
+
+
+//str2のパース結果
+    try {
+        console.log(JSON.parse(str2))
+    } catch (error) {
+        console.log(error)
+    //kokoniriyu wo kakude
+
+    }   
+
+
+    try {
+        console.log(JSON.parse(str3))
+    } catch (error) {
+        console.log(error)
+    //kokoniriyu wo kakude
+
+    }
+
+try {
+    console.log(JSON.parse(str4))
+} catch (error) {
+    console.log(error)
+    //kokoniriyu wo kakude
+
+}
+
+try {
+    console.log(JSON.parse(str5))
+} catch (error) {
+    console.log(error)
+    //kokoniriyu wo kakude
+
+}
+
+try {
+    console.log(JSON.parse(str6))
+} catch (error) {
+    console.log(error)
+    //kokoniriyu wo kakude
+
+}
